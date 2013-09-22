39567d11bb09935d43445e2f38539f87d6df5c05
zoechi <gzoechi@gmail.com>
2013-09-22 12:37:57 +0200

    bind a method to a custom attribute declaratively

--- web/custom_element_with_custom_attribute_binding/index.dart
+++ web/custom_element_with_custom_attribute_binding/index.dart
@@ -4,6 +4,19 @@ import 'package:polymer/polymer.dart';
 class Model extends Object with ObservableMixin {
   @observable
   String value = "Initial value";
+  
+  // bind an event method to a custom attribute
+  var myBlur;
+  
+  Model() {
+    myBlur = onBlur;
+  }
+  
+  void onBlur(e) {
+    // this prints the wrong value because value is not updated
+    // see comment in index.html
+    window.alert(value);
+  }
 }
 
 main() {
--- web/custom_element_with_custom_attribute_binding/my_element.dart
+++ web/custom_element_with_custom_attribute_binding/my_element.dart
@@ -3,4 +3,12 @@ import 'package:polymer/polymer.dart';
 @CustomTag('my-element')
 class MyElement extends PolymerElement with ObservableMixin {
   @observable String value;
+  
+  // bind an event method to a custom attribute
+  var myblur;
+  
+  inserted(){
+    super.inserted();
+    this.onBlur.listen((e) => myblur(e));
+  }
 }
